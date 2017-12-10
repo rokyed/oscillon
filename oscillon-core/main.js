924eec6788ee1ac9dcd7a7827a0ca2ba323aa44b
Rokyed <rokyed@gmail.com>
2017-12-10 12:50:36 -0500

    yayyye

--- oscillon-core/main.js
+++ oscillon-core/main.js
@@ -7,63 +7,54 @@ var audioContext;
 var oscilloscope;
 
 function init() {
-  oscilloscope = AudioOscilloscope(document.getElementById('canvas'), {
-	canvas: {
-	  width: function() {
-		return window.innerWidth;
-	  },
-	  height: 400
-	},
-	canvasContext: {
-	  lineWidth: 2,
-	  fillStyle: 'rgb(0,0,0)',
-	  strokeStyle: 'green'
+	oscilloscope = AudioOscilloscope(document.getElementById('canvas'), {
+		canvas: {
+			width: function() {
+				return window.innerWidth;
+			},
+			height: function() {
+				return window.innerHeight;
+			}
+		},
+		canvasContext: {
+			lineWidth: 1,
+			fillStyle: 'rgb(0,0,0)',
+			strokeStyle: 'green'
+		},
+		bufferLength: 500
+		});
+
+		// oscilloscope.on('drawFrame', function(osc) {
+		// ar c = osc.canvas;
+		// ar ctx = osc.canvasContext;
+		// ar gradient = ctx.createLinearGradient(0,0,c.width,0);
+		// radient.addColorStop(0, randomColor());
+		// radient.addColorStop(0.5, randomColor());
+		// radient.addColorStop(1, randomColor());
+		// tx.strokeStyle = gradient;
+		// });
+
+		oscilloscope.draw();
+
+		try {
+			audioContext = new AudioContext();
+		} catch (e) {
+			alert('No web audio support in this browser!');
+		}
+
+		navigator.getUserMedia({
+			audio: true
+		}, startUserMedia, userMediaError);
 	}
-  });
 
-  oscilloscope.on('drawFrame', function(osc) {
-	var c = osc.canvas;
-	var ctx = osc.canvasContext;
-	var gradient = ctx.createLinearGradient(0,0,c.width,0);
-	gradient.addColorStop(0, randomColor());
-	gradient.addColorStop(0.5, randomColor());
-	gradient.addColorStop(1, randomColor());
-	ctx.strokeStyle = gradient;
-  });
+	function startUserMedia(stream) {
+		var streamSource = audioContext.createMediaStreamSource(stream);
+		oscilloscope.addSource(streamSource);
+		oscilloscope.analyzer.fftSize = 32
+	}
 
-  oscilloscope.draw();
+	function userMediaError(error) {
+		console.error(error);
+	}
 
-  try {
-	audioContext = new AudioContext();
-  } catch (e) {
-	alert('No web audio support in this browser!');
-  }
-
-  navigator.getUserMedia({
-	audio: true
-  }, startUserMedia, userMediaError);
-}
-
-function startUserMedia(stream) {
-  var streamSource = audioContext.createMediaStreamSource(stream);
-  oscilloscope.addSource(streamSource);
-}
-
-function userMediaError(error) {
-  console.error(error);
-}
-
-function randomColor() {
-  var i = 3;
-  var colors = [];
-  while(i--) {
-	colors.push(random(0,255));
-  }
-  return 'rgb(' + colors.join(',') + ')';
-}
-
-function random(a,b) {
-  return ((Math.random() * (b - a + 1) + a)|0);
-}
-
-window.onload = init;
+	window.onload = init;
